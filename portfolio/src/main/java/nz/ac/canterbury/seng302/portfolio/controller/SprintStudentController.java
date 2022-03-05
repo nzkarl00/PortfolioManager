e3da7db28e69ada3da710ee584bc740fdf277f87
Lachlan Alsop <lra63@uclive.ac.nz>
2022-03-05 13:06:54 +1300

    did some controller stuff to get the pages working

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/SprintStudentController.java
@@ -0,0 +1,24 @@
+package nz.ac.canterbury.seng302.portfolio.controller;
+
+import nz.ac.canterbury.seng302.portfolio.service.GreeterClientService;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import nz.ac.canterbury.seng302.shared.identityprovider.ClaimDTO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SprintStudentController {
+
+  @GetMapping("/sprintStudent")
+  public String sprintTeacher(Model model) {
+    model.addAttribute("message", "Hello from the controller");
+    return "sprintStudent";
+  }
+
+}
