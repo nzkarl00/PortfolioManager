70ef91e7e71819ade5ce499def6089c09c06a7b7
Zoher Hussein <zhu45@uclive.ac.nz>
2022-03-09 00:33:19 +1300

    Added the same information as the student project page

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProjectPageTeacherController.java
@@ -0,0 +1,22 @@
+package nz.ac.canterbury.seng302.portfolio.controller;
+
+import nz.ac.canterbury.seng302.portfolio.service.GreeterClientService;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import nz.ac.canterbury.seng302.shared.identityprovider.ClaimDTO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ProjectPageTeacherController {
+
+    @GetMapping("/projectPageTeacher")
+    public String projectPageStudentController(Model model) {
+        return "projectPageTeacher";
+    }
+}
