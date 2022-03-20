30918fbc41503d3256d7614c3ddd2092938db333
Lane Edwards-Brown <led43@uclive.ac.nz>
2022-03-20 20:05:22 +1300

    Fixed some grammer errors

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/LandingController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/LandingController.java
@@ -1,6 +1,9 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
+import nz.ac.canterbury.seng302.portfolio.model.Project;
 import nz.ac.canterbury.seng302.portfolio.service.GreeterClientService;
+import nz.ac.canterbury.seng302.portfolio.service.ProjectService;
+import nz.ac.canterbury.seng302.portfolio.service.SprintService;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
 import nz.ac.canterbury.seng302.shared.identityprovider.ClaimDTO;
 
@@ -15,11 +18,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LandingController {
 
+
+  @Autowired
+  private ProjectService projectService;
+  @Autowired
+  private SprintService sprintService;
+
   @GetMapping("/landing")
   public String landing(
       @AuthenticationPrincipal AuthState principal,
       Model model
-  ) {
+  ) throws Exception {
+
+    Project project = projectService.getProjectById(0);
+    model.addAttribute("project", project);
+
     return "landing";
   }
 }
