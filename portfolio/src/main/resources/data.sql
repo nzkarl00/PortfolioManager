c6ab2d4971f9bcc325e5ebeacbb23a9973f14f8d
Hugo Reeves <hre56@uclive.ac.nz>
2022-03-25 14:44:09 +1300

    Delete data.sql from portfolio

--- portfolio/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
--- INSERT INTO account_profile VALUES (
---     1,
---     'USER',
---     '$2a$12$Q/X/hcw4ijD3g7R8UJjHVelxtUoBLceA.qiLwtPw3.gYbJJNXXtha',
---     '2022-02-03',
---     '',
---     'user@domain.com',
---     'identityprovider/src/main/resources/images/default_account_icon.png'
--- );
