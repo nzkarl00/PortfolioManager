d79eb56e4c5bad5e482b6f5bd5cf537061ab11bd
Karl Moore <krm97@uclive.ac.nz>
2022-03-27 00:29:12 +1300

    Added javadoc for the AccountServerServiceTests

--- identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/service/AccountServerServiceTests.java
+++ identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/service/AccountServerServiceTests.java
@@ -5,33 +5,47 @@ import nz.ac.canterbury.seng302.identityprovider.model.AccountProfile;
 import nz.ac.canterbury.seng302.identityprovider.model.AccountProfileRepository;
 import nz.ac.canterbury.seng302.shared.identityprovider.UserRegisterRequest;
 import nz.ac.canterbury.seng302.shared.identityprovider.UserRegisterResponse;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class AccountServerServiceTests {
 
+    /**
+     * The account server service we are testing in this class
+     */
     @Autowired
     private static AccountServerService ass = new AccountServerService();
 
-    private AccountServerService testService = Mockito.mock(AccountServerService.class);
+    /**
+     * Mocked repo so the call to store registered users in the database does nothing instead
+     */
     @Autowired
-    static
-    AccountProfileRepository repo = Mockito.mock(AccountProfileRepository.class);
+    static AccountProfileRepository repo = Mockito.mock(AccountProfileRepository.class);
 
+    /**
+     * Mocked account service so database checks can be replaced with fixed results
+     */
     private static AccountService as = Mockito.mock(AccountService.class);
 
+    /**
+     * Mocked stream observer to parse response as a replacement for the portfolio
+     */
     private StreamObserver<UserRegisterResponse> testObserver = mock(StreamObserver.class);
 
     private static AccountProfile testAccountProfile = new AccountProfile();
 
+
+    // Different parameters are required for all of these where they are checked, else the Mockito when/then calls break. That's why there are three.
+    // If you want to add new tests in here, you must also make a new request.
+    /**
+     * Request for test number one
+     */
     private UserRegisterRequest testRequest1= UserRegisterRequest.newBuilder().setUsername("testusername1")
             .setPassword("testpassword")
             .setFirstName("testfirst")
@@ -40,6 +54,9 @@ public class AccountServerServiceTests {
             .setEmail("test@email1")
             .build();
 
+    /**
+     * Request for test number two
+     */
     private UserRegisterRequest testRequest2= UserRegisterRequest.newBuilder().setUsername("testusername2")
             .setPassword("testpassword")
             .setFirstName("testfirst")
@@ -48,6 +65,9 @@ public class AccountServerServiceTests {
             .setEmail("test@email2")
             .build();
 
+    /**
+     * Request for test number three
+     */
     private UserRegisterRequest testRequest3= UserRegisterRequest.newBuilder().setUsername("testusername3")
             .setPassword("testpassword")
             .setFirstName("testfirst")
@@ -56,12 +76,19 @@ public class AccountServerServiceTests {
             .setEmail("test@email3")
             .build();
 
+    /**
+     * Setup to replace the autowired instances of these with the mocks
+     */
     @BeforeEach
     void setup() {
         ass.repo = repo;
         ass.accountService = as;
     }
 
+    /**
+     * Tests an account with existing username. It calls a register request with the mocked observer, checks for completion and sending, then reads the response.
+     * @throws Exception
+     */
     @Test
     void createAccountWithExistingUsername() throws Exception {
         when(as.getAccountByUsername("testusername1")).thenReturn(new AccountProfile()); // Simulates username already found
@@ -74,6 +101,10 @@ public class AccountServerServiceTests {
         assertEquals(false, response.getIsSuccess());
     }
 
+    /**
+     * Tests an account with existing email. It calls a register request with the mocked observer, checks for completion and sending, then reads the response.
+     * @throws Exception
+     */
     @Test
     void createAccountWithExistingEmail() throws Exception {
         when(as.getAccountByUsername("testusername2")).thenThrow(new Exception("Account profile not found")); // Simulates username not found
@@ -86,6 +117,10 @@ public class AccountServerServiceTests {
         assertEquals(false, response.getIsSuccess());
     }
 
+    /**
+     * Tests an account with valid details. It calls a register request with the mocked observer, checks for completion and sending, then reads the response.
+     * @throws Exception
+     */
     @Test
     void createAccountWithValidAttributes() throws Exception {
         when(as.getAccountByUsername("testusername3")).thenThrow(new Exception("Account profile not found")); // Simulates username not found
