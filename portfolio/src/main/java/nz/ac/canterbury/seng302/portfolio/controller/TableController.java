ea33d0e16af13c2dfa596b36fd6f9da2773ba658
Lachlan Alsop <lra63@uclive.ac.nz>
2022-04-18 10:59:00 +0000

    Working paginate users?

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/TableController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/TableController.java
@@ -16,6 +16,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.Optional;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -28,22 +34,41 @@ public class TableController {
     @Autowired
     private AccountClientService accountClientService;
 
+    private int start = 0;
+    private int step = 50;
+    private int currentPage = 0;
     Integer sortMode = 0;
     Integer ascDesc = 0;
     Boolean isSorted = false;
     List<User> users = new ArrayList<User>();
 
     /**
-     * control the displaying of account details
+     * The controller mapping of the User table
      * @param principal the auth token
-     * @param model
-     * @return string of where to go next
+     * @param model the model add templating attributes to
+     * @param move param to move back or forward a page
+     * @return String corresponding to html page
      */
     @GetMapping("/user-list")
     public String account(
-            @AuthenticationPrincipal AuthState principal,
-            Model model
-    ) {
+        @AuthenticationPrincipal AuthState principal,
+        Model model,
+        @RequestParam("move") Optional<String> move) {
+        // Note the pagination was taken from https://www.baeldung.com/spring-thymeleaf-pagination
+        // Update, a bit of philosophy from the above link
+        // and some details from https://stackoverflow.com/questions/5095887/how-do-i-pass-a-url-with-multiple-parameters-into-a-url
+        // and https://stackoverflow.com/questions/46216134/thymeleaf-how-to-make-a-button-link-to-another-html-page
+        String movePage = move.orElse("");
+
+        // Receive the forward or backward call from the button and iterate current page
+        if (movePage.equals("forward")) {
+            currentPage++;
+        } else if (movePage.equals("back")) {
+            if (currentPage > 0) {
+                currentPage--;
+            }
+        }
+        start = currentPage * step;
 
         users.clear();
         isSorted = false;
@@ -53,10 +78,14 @@ public class TableController {
         userReply = accountClientService.getUserById(id); // Get the user
 
         model.addAttribute("date", DateParser.displayDate(userReply));
+        model.addAttribute("start", start);
+        model.addAttribute("currentPage", currentPage);
 
-        PaginatedUsersResponse response = accountClientService.getPaginatedUsers(50, 0, "name");
-
-        OrderList(response);
+        PaginatedUsersResponse response = accountClientService.getPaginatedUsers(step, start, "name");
+        List<User> users = new ArrayList<>();
+        for (UserResponse userResponse : response.getUsersList()) { // loop through the given response containing UserResponses
+            users.add(new User(userResponse)); // pass the UserResponse to the User constructor which builds the appropriate user
+        }
         model.addAttribute("users", users);
         return "userList";
     }
