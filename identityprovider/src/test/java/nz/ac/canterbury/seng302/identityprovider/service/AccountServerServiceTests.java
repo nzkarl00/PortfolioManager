e2dfef7261addbb02006437043f68b71111e2db1
Lachlan Alsop <lra63@uclive.ac.nz>
2022-04-28 17:57:26 +1200

    paginated users is sufficiently tested now

--- identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/service/AccountServerServiceTests.java
+++ identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/service/AccountServerServiceTests.java
@@ -3,12 +3,17 @@ package nz.ac.canterbury.seng302.identityprovider.service;
 import io.grpc.stub.StreamObserver;
 import nz.ac.canterbury.seng302.identityprovider.model.AccountProfile;
 import nz.ac.canterbury.seng302.identityprovider.model.AccountProfileRepository;
+import nz.ac.canterbury.seng302.identityprovider.model.Role;
+import nz.ac.canterbury.seng302.identityprovider.model.RolesRepository;
 import nz.ac.canterbury.seng302.shared.identityprovider.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -29,6 +34,12 @@ public class AccountServerServiceTests {
     static AccountProfileRepository repo = Mockito.mock(AccountProfileRepository.class);
 
     /**
+     * Mocked repo so the call to store registered users in the database does nothing instead
+     */
+    @Autowired
+    static RolesRepository roleRepo = mock(RolesRepository.class);
+
+    /**
      * Mocked account service so database checks can be replaced with fixed results
      */
     private static Account as = Mockito.mock(Account.class);
@@ -38,7 +49,15 @@ public class AccountServerServiceTests {
      */
     private StreamObserver<UserRegisterResponse> testObserver = mock(StreamObserver.class);
 
-    private static AccountProfile testAccountProfile = new AccountProfile();
+    // setup test account profile and a list of said profiles to mock the repo with
+    private static AccountProfile testAccountProfile = new AccountProfile("test username",
+        "test hash", new Date(), "test bio", "test email",
+        "test/photopath/", "firstname", "lastname", "pronouns");
+    private static ArrayList<AccountProfile> testProfiles = new ArrayList<>();
+
+    // setup test role and a list of said roles to mock the repo with
+    private static Role testRole = new Role(testAccountProfile, "1student");
+    private static ArrayList<Role> testRoles = new ArrayList<>();
 
 
     // Different parameters are required for all of these where they are checked, else the Mockito when/then calls break. That's why there are three.
@@ -81,6 +100,7 @@ public class AccountServerServiceTests {
      */
     @BeforeEach
     void setup() {
+        ass.roleRepo = roleRepo;
         ass.repo = repo;
         ass.accountService = as;
     }
@@ -143,15 +163,111 @@ public class AccountServerServiceTests {
      */
     @Test
     void getPaginatedUsersTest() {
+        // set up the profile repo findAll result with a valid account profile
+        testProfiles = new ArrayList<>();
+        testProfiles.add(testAccountProfile);
+        testRoles.add(testRole);
+        testAccountProfile.addRole(testRole);
+
+        // mock the repo response
+        when(repo.findAll()).thenReturn(testProfiles);
+
+        // make the request
         GetPaginatedUsersRequest.Builder request = GetPaginatedUsersRequest.newBuilder()
             .setLimit(50)
-            .setOffset(0)
-            .setOrderBy("name");
+            .setOffset(0);
+        
+        // run the method we are testing with the mocked observer
         ass.getPaginatedUsers(request.build(), testPaginatedObserver);
+
+        // test we are receiving and processing correctly
         verify(testPaginatedObserver, times(1)).onCompleted();
         ArgumentCaptor<PaginatedUsersResponse> captor = ArgumentCaptor.forClass(PaginatedUsersResponse.class);
         verify(testPaginatedObserver, times(1)).onNext(captor.capture());
         PaginatedUsersResponse response = captor.getValue();
-        assertTrue(response.getUsersCount() >= 0); //TODO make it actually return data in the repo? can this be done?
+
+        // for debugging
+        for (UserResponse userResponse : response.getUsersList()) {
+            System.out.println(userResponse);
+        }
+
+        // make sure we get the user we put in during set up
+        assertEquals(1, response.getUsersCount());
+    }
+
+    /**
+     * Tests the grpc call to get paginated users that don't exist that "far down" in the repo
+     */
+    @Test
+    void getPaginatedUsersOutOfRangeTest() {
+        // set up the profile repo findAll result with a valid account profile
+        testProfiles = new ArrayList<>();
+        testProfiles.add(testAccountProfile);
+        testRoles.add(testRole);
+        testAccountProfile.addRole(testRole);
+
+        // mock the repo response
+        when(repo.findAll()).thenReturn(testProfiles);
+
+        // make the request
+        GetPaginatedUsersRequest.Builder request = GetPaginatedUsersRequest.newBuilder()
+            .setLimit(50)
+            .setOffset(50);
+
+        // run the method we are testing with the mocked observer
+        ass.getPaginatedUsers(request.build(), testPaginatedObserver);
+
+        // test we are receiving and processing correctly
+        verify(testPaginatedObserver, times(1)).onCompleted();
+        ArgumentCaptor<PaginatedUsersResponse> captor = ArgumentCaptor.forClass(PaginatedUsersResponse.class);
+        verify(testPaginatedObserver, times(1)).onNext(captor.capture());
+        PaginatedUsersResponse response = captor.getValue();
+
+        // for debugging
+        for (UserResponse userResponse : response.getUsersList()) {
+            System.out.println(userResponse);
+        }
+
+        // make sure we get the user we put in during set up
+        assertEquals(0, response.getUsersCount());
+    }
+
+    /**
+     * Tests the grpc call to get paginated users that do exist that "far down" in the repo
+     */
+    @Test
+    void getPaginatedUsersInRangeTest() {
+        // set up the profile repo findAll result with valid account profiles
+        testProfiles = new ArrayList<>();
+        for (int i = 0; i < 51; i++) {
+            testProfiles.add(testAccountProfile);
+        }
+        testRoles.add(testRole);
+        testAccountProfile.addRole(testRole);
+
+        // mock the repo response
+        when(repo.findAll()).thenReturn(testProfiles);
+
+        // make the request
+        GetPaginatedUsersRequest.Builder request = GetPaginatedUsersRequest.newBuilder()
+            .setLimit(50)
+            .setOffset(50);
+
+        // run the method we are testing with the mocked observer
+        ass.getPaginatedUsers(request.build(), testPaginatedObserver);
+
+        // test we are receiving and processing correctly
+        verify(testPaginatedObserver, times(1)).onCompleted();
+        ArgumentCaptor<PaginatedUsersResponse> captor = ArgumentCaptor.forClass(PaginatedUsersResponse.class);
+        verify(testPaginatedObserver, times(1)).onNext(captor.capture());
+        PaginatedUsersResponse response = captor.getValue();
+
+        // for debugging
+        for (UserResponse userResponse : response.getUsersList()) {
+            System.out.println(userResponse);
+        }
+
+        // make sure we get the user we put in during set up
+        assertEquals(1, response.getUsersCount());
     }
 }
