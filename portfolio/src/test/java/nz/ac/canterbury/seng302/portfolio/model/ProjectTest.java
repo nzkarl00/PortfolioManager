6817fe1d075a9ec09201d1415e8f6168993110ac
Zoher Hussein <zhu45@uclive.ac.nz>
2022-05-05 23:39:34 +1200

    Finshed tests for project in portfolio model #test

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/model/ProjectTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/model/ProjectTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProjectTest {
 
     @Test
-    void projectInStringFormatTest() {
+    void projectInLocalDateFormatTest() {
 
         // formatting for comparing the dates
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -42,9 +42,6 @@ class ProjectTest {
     @Test
     void projectInDateFormatTest() {
 
-        // formatting for comparing the dates
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-
         // create variables to be passed to the constructors
         String expectedName = "Project 2022";
         String expectedDescription = "this project is about a new sprint";
@@ -62,6 +59,53 @@ class ProjectTest {
         assertEquals(expectedStartDate, actualProject.getStartDate());
         assertEquals(expectedEndDate, actualProject.getEndDate());
 
+    }
+
+    @Test
+    void projectInStringFormatTest() {
+
+        // formatting for comparing the dates
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        // create variables to be passed to the constructors
+        String expectedName = "Project 2022";
+        String expectedDescription = "this project is about a new sprint";
+        String expectedStartDate = LocalDate.now().toString();
+        String expectedEndDate = LocalDate.now().plusMonths(8).toString();
+
+
+        // instance being made
+        Project actualProject = new Project(expectedName, expectedDescription, expectedStartDate, expectedEndDate);
+
+        // convert dates to localDate format for comparing
+        String actualStartDate = simpleDateFormat.format(actualProject.getStartDate());
+        String actualEndDate = simpleDateFormat.format(actualProject.getEndDate());
+
+        // check to see if expected and actual are equal
+        assertEquals(expectedName, actualProject.getName());
+        assertEquals(expectedDescription, actualProject.getDescription());
+        assertEquals(expectedStartDate, actualStartDate);
+        assertEquals(expectedEndDate, actualEndDate);
+
+    }
+
+    @Test
+    void toStringTest() {
+
+        // create variables to be passed to the constructors
+        Calendar calendar = Calendar.getInstance();
+        Date expectedStartDate = calendar.getTime();
+        calendar.add(Calendar.MONTH, 8);
+        Date expectedEndDate = calendar.getTime();
+
+        // instance being made for a new project
+        Project actualProject = new Project("Project 2022", "test description", expectedStartDate, expectedEndDate);
+
+        String expectedFormat = String.format(
+                "Project[id=%d, projectName='Project 2022', projectStartDate='%s', projectEndDate='%s', projectDescription='test description']",
+                actualProject.getId(), actualProject.getStartDate(), actualProject.getEndDate());
+        // check to see if expected and actual are equal
+        assertEquals(expectedFormat, actualProject.toString());
     }
 
 }
