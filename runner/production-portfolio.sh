a8f7e526df121008db5a0ebba9d9d30bac98573a
Hugo Reeves <hre56@uclive.ac.nz>
2022-05-09 10:12:08 +1200

    Application properties for all environments

--- runner/production-portfolio.sh
+++ runner/production-portfolio.sh
@@ -2,9 +2,8 @@ fuser -k 10501/tcp || true
 
 source production-portfolio-env.sh
 
-echo $MARIADB_USERNAME
-
-java -jar production-portfolio/libs/portfolio-0.0.1-SNAPSHOT.jar \
+env SPRING_PROFILES_ACTIVE=prod \
+    java -jar production-portfolio/libs/portfolio-0.0.1-SNAPSHOT.jar \
     --server.port=10501 \
     --spring.application.name=portfolio \
     --grpc.client.identity-provider-grpc-server.address=static://127.0.0.1:10500 \
