3bca078828bd340a972f5052240ea73545c0480d
Hugo Reeves <hre56@uclive.ac.nz>
2022-05-11 21:47:31 +1200

    Removing get mapping

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/controller/ProfilePhotoController.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/controller/ProfilePhotoController.java
@@ -13,7 +13,6 @@ import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.File;
 import java.io.IOException;
