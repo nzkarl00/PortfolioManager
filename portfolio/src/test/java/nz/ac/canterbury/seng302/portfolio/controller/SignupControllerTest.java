6e261557e7cd6e26e2ad2189402dfee68252c512
Toby Morgan <tmo89@uclive.ac.nz>
2022-05-11 16:06:54 +1200

    Signup controller test now functional, tests for getmapping and blue sky scenario post mapping

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/SignupControllerTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/SignupControllerTest.java
@@ -1,69 +1,48 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
-import java.io.IOException;
-import java.security.Principal;
-import java.util.ArrayList;
-
 import com.google.protobuf.Timestamp;
-import com.google.rpc.context.AttributeContext;
-import nz.ac.canterbury.seng302.portfolio.authentication.AuthenticationClientInterceptor;
-import nz.ac.canterbury.seng302.portfolio.authentication.JwtAuthenticationFilter;
-import nz.ac.canterbury.seng302.portfolio.authentication.JwtAuthenticationToken;
 import nz.ac.canterbury.seng302.portfolio.service.AccountClientService;
-import nz.ac.canterbury.seng302.portfolio.service.AuthStateInformer;
 import nz.ac.canterbury.seng302.portfolio.service.AuthenticateClientService;
-import nz.ac.canterbury.seng302.shared.identityprovider.*;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserRegisterResponse;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserRole;
 import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.core.MethodParameter;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.bind.support.WebDataBinderFactory;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.ModelAndViewContainer;
-import org.springframework.web.servlet.HandlerInterceptor;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
-import static org.mockito.MockitoAnnotations.initMocks;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = SignupController.class)
 @AutoConfigureMockMvc(addFilters = false)
 public class SignupControllerTest {
+
+    private int userID = 1;
+    private String username = "username";
+    private String password = "password";
+    private String firstname = "firstname";
+    private String lastname = "lastname";
+    private String pronouns = "";
+    private String email = "email@email.email";
+
+    String successCode = "userRegisterResponse Successful";
+
+    UserRegisterResponse userRegisterResponse = UserRegisterResponse.newBuilder()
+            .setIsSuccess(true)
+            .setNewUserId(userID)
+            .setMessage(successCode)
+            .build();
 
     @Autowired
     private MockMvc mockMvc;
@@ -71,20 +50,37 @@ public class SignupControllerTest {
     @MockBean
     AccountClientService accountClientService;
 
+    @MockBean
+    AuthenticateClientService authenticateClientService;
+
     @Before
     public void setup() {
-        mockMvc = MockMvcBuilders.standaloneSetup(SignupController.class)
-                .build();
+        mockMvc = MockMvcBuilders.standaloneSetup(SignupController.class).build();
     }
 
     @Test
-    public void signupSanityCheck() throws Exception {
+    public void signupGetMapping() throws Exception {
         mockMvc.perform(get("/signup"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("signup"))
-                .andExpect(MockMvcResultMatchers.model().attribute("errorShow", "display:none;"))
-                .andExpect(MockMvcResultMatchers.model().attribute("successShow", "display:none;"))
-                .andExpect(MockMvcResultMatchers.model().attribute("successCode", "successCode"));
+                .andExpect(MockMvcResultMatchers.model().attribute("successCode", successCode));
     }
 
+    @Test
+    public void signupBlueSky() throws Exception {
+        when(accountClientService.register(username, password, firstname, lastname, pronouns, email))
+                .thenReturn(userRegisterResponse);
+
+        mockMvc.perform(post("/signup")
+            .param("username", username)
+            .param("password", password)
+            .param("passwordConfirm", password)
+            .param("firstname", firstname)
+            .param("lastname", lastname)
+            .param("pronouns", pronouns)
+            .param("email", email)
+        )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:signup"));
+    }
 }
