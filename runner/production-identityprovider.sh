f42ca59f17ee467a0ab77db36172c6e459a7b290
Hugo Reeves <hre56@uclive.ac.nz>
2022-05-17 09:08:45 +1200

    Set GRPC port to non 500's as per wiki

--- runner/production-identityprovider.sh
+++ runner/production-identityprovider.sh
@@ -1,6 +1,7 @@
 fuser -k 10500/tcp || true
+fuser -k 10502/tcp || true
 
 source production-identityprovider-env.sh
 
 env SPRING_PROFILES_ACTIVE=prod \
-  java -jar production-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar --spring.application.name=identity-provider --grpc.server.port=10500
+  java -jar production-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar --spring.application.name=identity-provider --grpc.server.port=10502
--- runner/production-portfolio.sh
+++ runner/production-portfolio.sh
@@ -6,7 +6,7 @@ env SPRING_PROFILES_ACTIVE=prod \
     java -jar production-portfolio/libs/portfolio-0.0.1-SNAPSHOT.jar \
     --server.port=10501 \
     --spring.application.name=portfolio \
-    --grpc.client.identity-provider-grpc-server.address=static://127.0.0.1:10500 \
+    --grpc.client.identity-provider-grpc-server.address=static://127.0.0.1:10502 \
     --grpc.client.identity-provider-grpc-server.enableKeepAlive=true \
     --grpc.client.identity-provider-grpc-server.keepAliveWithoutCalls=true \
-    --grpc.client.identity-provider-grpc-server.negotiationType=plaintext 
+    --grpc.client.identity-provider-grpc-server.negotiationType=plaintext
