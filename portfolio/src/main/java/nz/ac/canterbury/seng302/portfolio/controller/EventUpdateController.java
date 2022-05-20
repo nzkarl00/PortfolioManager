36e11cb3b4c70f9ab48c6122e961a4267f9a71ef
Lachlan Alsop <lra63@uclive.ac.nz>
2022-05-20 15:28:55 +1200

    redoing as it didn't save lat time for some weird reason

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EventUpdateController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EventUpdateController.java
@@ -1,4 +1,23 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
+import nz.ac.canterbury.seng302.portfolio.model.EventUpdate;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
 public class EventUpdateController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public EventUpdate greeting(EventUpdate message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new EventUpdate("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+    @GetMapping("/hello")
+    public String getHtml() {
+        return "socketTest";
+    }
 }
--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/EventUpdate.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/EventUpdate.java
@@ -1,4 +1,20 @@
 package nz.ac.canterbury.seng302.portfolio.model;
 
 public class EventUpdate {
+    private String name;
+
+    public EventUpdate() {
+    }
+
+    public EventUpdate(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
