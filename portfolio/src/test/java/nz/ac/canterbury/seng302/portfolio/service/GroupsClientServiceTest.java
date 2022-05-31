3dd42a2946f2902d4b7db6d84d23e875813eab12
Zoher Hussein <zhu45@uclive.ac.nz>
2022-05-31 22:53:03 +1200

    Added portfolio testing (client side) for creating and deleting groups #test

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/service/GroupsClientServiceTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/service/GroupsClientServiceTest.java
@@ -1,13 +1,15 @@
 package nz.ac.canterbury.seng302.portfolio.service;
 
-import nz.ac.canterbury.seng302.shared.identityprovider.UserAccountServiceGrpc;
+import nz.ac.canterbury.seng302.shared.identityprovider.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class GroupsClientServiceTest {
-
 
     /**
      * The group server service we are testing in this class
@@ -15,19 +17,61 @@ public class GroupsClientServiceTest {
     @Autowired
     private static GroupsClientService groupClientService = new GroupsClientService();
 
-//    /**
-//     * The stub to contact for and mock back grpc responses
-//     */
-//    @Autowired
-//    private GroupsServiceServiceGrpc. UserAccountServiceGrpc.UserAccountServiceBlockingStub accountServiceStub = mock(UserAccountServiceGrpc.UserAccountServiceBlockingStub.class);
-//
-//    /**
-//     * Setup to replace the autowired instances of these with the mocks
-//     */
-//    @BeforeEach
-//    void setup() {
-//        accountClientService.accountServiceStub = accountServiceStub;
-//    }
-//
+    /**
+     * The stub to contact for and mock back grpc responses
+     */
+    @Autowired
+    private GroupsServiceGrpc.GroupsServiceBlockingStub groupServiceStub = mock(GroupsServiceGrpc.GroupsServiceBlockingStub.class);
+
+    /**
+     * Setup to replace the autowired instances of these with the mocks
+     */
+    @BeforeEach
+    void setup() {
+        groupClientService.groupServiceStub = groupServiceStub;
+    }
+
+    /**
+     * Test is for creating a group
+     */
+    @Test
+    void createGroup() {
+        CreateGroupResponse expected = CreateGroupResponse.newBuilder()
+                .setIsSuccess(true)
+                .setMessage("group has been successfully been made")
+                .setNewGroupId(1)
+                .build();
+
+        CreateGroupRequest expectedInput = CreateGroupRequest.newBuilder()
+                .setLongName("The Owl Group")
+                .setShortName("TOG")
+                .build();
+
+        when(groupServiceStub.createGroup(expectedInput)).thenReturn(expected);
+        CreateGroupResponse actual = groupClientService.create("TOG", "The Owl Group");
+        assertEquals(expected, actual);
+
+    }
+
+    /**
+     * Test is for deleting a group
+     */
+    @Test
+    void deleteGroup() {
+        DeleteGroupResponse expected = DeleteGroupResponse.newBuilder()
+                .setIsSuccess(true)
+                .setMessage("group has been successfully deleted")
+                .build();
+
+
+        DeleteGroupRequest expectedInput = DeleteGroupRequest.newBuilder()
+                .setGroupId(1)
+                .build();
+
+        when(groupServiceStub.deleteGroup(expectedInput)).thenReturn(expected);
+        DeleteGroupResponse actual = groupClientService.delete(1);
+        assertEquals(expected, actual);
+
+    }
 
 }
