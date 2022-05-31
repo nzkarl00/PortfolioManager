bf159bf19c95689c81a9e58d3eeb9a5a24f9dd76
Zoher Hussein <zhu45@uclive.ac.nz>
2022-05-31 12:57:11 +1200

    Looking at the repo and trying to figure the tests. Made final tests for creating groups #test

--- identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/service/GroupsServerServiceTest.java
+++ identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/service/GroupsServerServiceTest.java
@@ -1,12 +1,10 @@
 package nz.ac.canterbury.seng302.identityprovider.service;
 
 import io.grpc.stub.StreamObserver;
-import nz.ac.canterbury.seng302.identityprovider.model.AccountProfileRepository;
 import nz.ac.canterbury.seng302.identityprovider.model.GroupRepository;
 import nz.ac.canterbury.seng302.identityprovider.model.Groups;
 import nz.ac.canterbury.seng302.shared.identityprovider.CreateGroupRequest;
 import nz.ac.canterbury.seng302.shared.identityprovider.CreateGroupResponse;
-import nz.ac.canterbury.seng302.shared.identityprovider.UserRegisterResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -44,6 +42,9 @@ class GroupsServerServiceTest {
      */
     private StreamObserver<CreateGroupResponse> testCreateObserver = mock(StreamObserver.class);
 
+    /**
+     * Tests to make a valid group
+     */
     @Test
     void createGroup_validNames() {
         List<Groups> groupsCheck = new ArrayList<>();
@@ -63,8 +64,11 @@ class GroupsServerServiceTest {
         assertTrue(response.getIsSuccess());
     }
 
+    /**
+     * Tests to make a duplicate group with existing long name
+     */
     @Test
-    void creatGroup_duplicateName_failTest() {
+    void creatGroup_duplicateLongName_failTest() {
         List<Groups> groupsCheck = new ArrayList<>();
         List<Groups> duplicate = new ArrayList<>();
         duplicate.add(new Groups("The Society of Pompous Rapscallions", "SPR"));
@@ -84,15 +88,20 @@ class GroupsServerServiceTest {
         assertFalse(response.getIsSuccess());
     }
 
+    /**
+     * Tests to make a duplicate group with existing short name
+     */
     @Test
-    void creatGroup_duplicateLongName_failTest() {
+    void creatGroup_duplicateShortName_failTest() {
+        List<Groups> groupsCheck = new ArrayList<>();
         List<Groups> duplicate = new ArrayList<>();
-        duplicate.add(new Groups("The Society of Pompous Rapscallions", "SPTR"));
-        when(groupRepo.findAllByLongName("The Society of Pompous Rapscallions")).thenReturn(duplicate);
+        duplicate.add(new Groups("The Society of Pompous Rapscallions", "SPR"));
+        when(groupRepo.findAllByLongName("The New Society of Pompous Rapscallions")).thenReturn(groupsCheck);
+        when(groupRepo.findAllByShortName("SPR")).thenReturn(groupsCheck);
         when(groupRepo.findAllByShortName("SPR")).thenReturn(duplicate);
         CreateGroupRequest invalidRequest = CreateGroupRequest.newBuilder()
-                .setLongName("The Society of Pompous Rapscallions")
-                .setShortName("TSPOR").build();
+                .setLongName("The New Society of Pompous Rapscallions")
+                .setShortName("SPR").build();
 
         gss.createGroup(invalidRequest, testCreateObserver);
 
@@ -103,26 +112,41 @@ class GroupsServerServiceTest {
         assertFalse(response.getIsSuccess());
     }
 
-//    @Test
-//    void creatGroup_duplicateShortName_failTest() {
-//        List<Groups> duplicate = new ArrayList<>();
-//        duplicate.add(new Groups("This is a new group", "rrt"));
-//        when(groupRepo.findAllByLongName("The Society of Pompous Rapscallions")).thenReturn(duplicate);
-//        when(groupRepo.findAllByShortName("rrt")).thenReturn(duplicate);
-//        CreateGroupRequest invalidRequest = CreateGroupRequest.newBuilder()
-//                .setLongName("The Society of Pompous Rapscallions")
-//                .setShortName("rrt").build();
-//
-//        gss.createGroup(invalidRequest, testCreateObserver);
-//
-//        verify(testCreateObserver, times(1)).onCompleted();
-//        ArgumentCaptor<CreateGroupResponse> captor = ArgumentCaptor.forClass(CreateGroupResponse.class);
-//        verify(testCreateObserver, times(1)).onNext(captor.capture());
-//        CreateGroupResponse response = captor.getValue();
-//        assertFalse(response.getIsSuccess());
-//    }
-//
-//
+    /**
+     * Tests to make a new group with the same short name as an existing groups long name
+     */
+    @Test
+    void creatGroup_duplicateShortNameInLongName_failTest() {
+        List<Groups> groupsCheck = new ArrayList<>();
+        List<Groups> duplicate = new ArrayList<>();
+        duplicate.add(new Groups("SPR", "SP"));
+        when(groupRepo.findAllByShortName("SPR")).thenReturn(groupsCheck);
+        when(groupRepo.findAllByLongName("SPR")).thenReturn(duplicate);
+        when(groupRepo.findAllByLongName("The Owl Group")).thenReturn(groupsCheck);
+        CreateGroupRequest invalidRequest = CreateGroupRequest.newBuilder()
+                .setLongName("The Owl Group")
+                .setShortName("SPR").build();
+
+        gss.createGroup(invalidRequest, testCreateObserver);
+
+        verify(testCreateObserver, times(1)).onCompleted();
+        ArgumentCaptor<CreateGroupResponse> captor = ArgumentCaptor.forClass(CreateGroupResponse.class);
+        verify(testCreateObserver, times(1)).onNext(captor.capture());
+        CreateGroupResponse response = captor.getValue();
+        assertFalse(response.getIsSuccess());
+    }
+
+    /**
+     * Test to delete valid group
+     */
+    @Test
+    void deleteGroup_validGroup() {
+
+
+
+
+
+    }
 
 
 }
