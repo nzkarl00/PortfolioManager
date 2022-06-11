d28da6ed63a7292c5178e3421a95521ff902d613
Lachlan Alsop <lra63@uclive.ac.nz>
2022-06-11 07:08:58 +0000

    added default users being made and some idp testing

---
+++ identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/authentication/AuthenticationValidatorUtilTest.java
@@ -0,0 +1,33 @@
+package nz.ac.canterbury.seng302.identityprovider.authentication;
+
+import nz.ac.canterbury.seng302.identityprovider.model.AccountProfile;
+import nz.ac.canterbury.seng302.identityprovider.model.Role;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import nz.ac.canterbury.seng302.shared.identityprovider.ClaimDTO;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthenticationValidatorUtilTest {
+
+    private static AccountProfile testAccountProfile = new AccountProfile("test username",
+        "test hash", new Date(), "test bio", "test email",
+        "test/photopath/", "firstname", "lastname", "pronouns");
+
+    private static Role testRole = new Role(testAccountProfile, "1student");
+
+    @Test
+    void validateTokenForAuthState_blueSky() {
+        testAccountProfile.addRoleTestingOnly(testRole);
+        JwtTokenUtil jwtTokenUtil = JwtTokenUtil.getInstance();
+        String token = jwtTokenUtil.generateTokenForUser(testAccountProfile);
+        AuthState authState = AuthenticationValidatorUtil.validateTokenForAuthState(token);
+        assertEquals("student", authState.getClaimsList().stream()
+            .filter(claim -> claim.getType().equals("role"))
+            .findFirst()
+            .map(ClaimDTO::getValue)
+            .orElse("NOT FOUND"));
+    }
+}
