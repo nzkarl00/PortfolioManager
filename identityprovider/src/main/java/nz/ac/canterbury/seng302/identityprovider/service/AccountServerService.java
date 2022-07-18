ec2b3279329aa7aa21e3ecee15f02d113260353e
Lane Edwards-Brown <led43@uclive.ac.nz>
2022-07-18 15:40:22 +1200

    Users are no longer added to the teacher group if they have multiple applicable roles (ie teacher and admin). Users will only be removed from teacher group if they have no applicable roles (ie teacher or admin).

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/AccountServerService.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/AccountServerService.java
@@ -432,9 +432,18 @@ public class AccountServerService extends UserAccountServiceImplBase{
                 roleRepo.deleteById(roleIdToRemove);
 
                 // if the role removal is a teacher also remove them from the teacher group
-                if (roleToRemove.equals(groupsServerService.TEACHER_ROLE)) {
-                    Groups teacherGroup = groupRepo.findAllByGroupShortName(groupsServerService.TEACHER_GROUP_NAME_SHORT).get(0);
-                    groupMembershipRepo.deleteByRegisteredGroupsAndRegisteredGroupUser(teacherGroup, user);
+                if (roleToRemove.equals(groupsServerService.TEACHER_ROLE) || role.equals(groupsServerService.ADMIN_ROLE)) {
+                    int isTeacher = 0;
+                    for ( Role currentRole: user.getRoles()) {
+                        System.out.println(currentRole.getPlainRole());
+                        if ((currentRole.getPlainRole().equals("admin")) || (currentRole.getPlainRole().equals("teacher"))) {
+                            isTeacher += 1;
+                        }
+                    }
+                    if (isTeacher < 2) {
+                        Groups teacherGroup = groupRepo.findAllByGroupShortName(groupsServerService.TEACHER_GROUP_NAME_SHORT).get(0);
+                        groupMembershipRepo.deleteByRegisteredGroupsAndRegisteredGroupUser(teacherGroup, user);
+                    }
 
                     // if there are no groups left for the user add them to Members Without A Group (MWAG)
                     if (groupMembershipRepo.findAllByRegisteredGroupUser(user).isEmpty()) {
@@ -468,6 +477,7 @@ public class AccountServerService extends UserAccountServiceImplBase{
         // if the role to add is a teacher, add them to the teacher group and remove from the members without a group.
         if (role.equals(groupsServerService.TEACHER_ROLE) || role.equals(groupsServerService.ADMIN_ROLE)) {
             Groups teacherGroup = groupRepo.findAllByGroupShortName(groupsServerService.TEACHER_GROUP_NAME_SHORT).get(0);
+            groupMembershipRepo.deleteByRegisteredGroupsAndRegisteredGroupUser(teacherGroup, user);
             groupMembershipRepo.save(new GroupMembership(user, teacherGroup));
 
             Groups noMembers = groupRepo.findAllByGroupShortName(groupsServerService.MWAG_GROUP_NAME_SHORT).get(0);
