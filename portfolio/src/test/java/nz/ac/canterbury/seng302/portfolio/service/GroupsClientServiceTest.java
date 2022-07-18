e6f92dc15bece94963571cbb09ac43f3e8ee635b
Lachlan Alsop <lra63@uclive.ac.nz>
2022-07-18 15:22:14 +1200

    refactoring the mess that was the portfolio group(s) services

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/service/GroupsClientServiceTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/service/GroupsClientServiceTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -168,6 +171,116 @@ public class GroupsClientServiceTest {
         ModifyGroupDetailsResponse actual = groupClientService.modifyGroup(1, "EditLong", "EditShort");
         assertEquals(expected, actual);
 
+    }
+
+    /**
+     * note this mainly tests the ability to make the request, and send it
+     */
+    @Test
+    void addUsersToGroupValidTest() {
+        ArrayList<Integer> UserIdsList = new ArrayList<>();
+        UserIdsList.add(1);
+        UserIdsList.add(2);
+        UserIdsList.add(3);
+        UserIdsList.add(4);
+        AddGroupMembersResponse expected = AddGroupMembersResponse.newBuilder().setIsSuccess(true).setMessage("Users: " + UserIdsList + " added.").build();
+        AddGroupMembersRequest.Builder request = AddGroupMembersRequest.newBuilder()
+                .setGroupId(1);
+
+        for (Integer userId : UserIdsList) {
+            request.addUserIds(userId);
+        }
+
+        AddGroupMembersRequest expectedInput = request.build();
+
+
+
+        when(groupServiceStub.addGroupMembers(expectedInput)).thenReturn(expected);
+        AddGroupMembersResponse actual = groupServiceStub.addGroupMembers(request.build());
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Tests that an invalid request does not send the same response
+     */
+    @Test
+    void addUsersToGroupInvalidTest() {
+        ArrayList<Integer> UserIdsList = new ArrayList<>();
+        UserIdsList.add(1);
+        UserIdsList.add(2);
+        UserIdsList.add(3);
+        UserIdsList.add(4);
+        AddGroupMembersResponse expected = AddGroupMembersResponse.newBuilder().setIsSuccess(true).setMessage("Users: " + UserIdsList + " added.").build();
+        AddGroupMembersRequest.Builder request = AddGroupMembersRequest.newBuilder()
+                .setGroupId(0);
+
+        for (Integer userId : UserIdsList) {
+            request.addUserIds(userId);
+        }
+
+        AddGroupMembersRequest expectedInput = request.build();
+        AddGroupMembersRequest fakeRequest = AddGroupMembersRequest.newBuilder().build();
+
+
+        when(groupServiceStub.addGroupMembers(expectedInput)).thenReturn(expected);
+        AddGroupMembersResponse actual = groupServiceStub.addGroupMembers(fakeRequest);
+        assertNotEquals(expected, actual);
+    }
+
+
+    /**
+     * note this mainly tests the ability to make the request, and send it
+     */
+    @Test
+    void removeUsersFromGroupValidTest() {
+        ArrayList<Integer> UserIdsList = new ArrayList<>();
+        UserIdsList.add(1);
+        UserIdsList.add(2);
+        UserIdsList.add(3);
+        UserIdsList.add(4);
+        RemoveGroupMembersResponse expected = RemoveGroupMembersResponse.newBuilder().setIsSuccess(true).setMessage("Users: " + UserIdsList + " removed.").build();
+        RemoveGroupMembersRequest.Builder request = RemoveGroupMembersRequest.newBuilder()
+                .setGroupId(1);
+
+        for (Integer userId : UserIdsList) {
+            request.addUserIds(userId);
+        }
+
+        RemoveGroupMembersRequest expectedInput = request.build();
+
+
+
+        when(groupServiceStub.removeGroupMembers(expectedInput)).thenReturn(expected);
+        RemoveGroupMembersResponse actual = groupServiceStub.removeGroupMembers(request.build());
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Tests that an invalid request does not send the same response
+     */
+    @Test
+    void removeUsersFromGroupInvalidTest() {
+        ArrayList<Integer> UserIdsList = new ArrayList<>();
+        UserIdsList.add(1);
+        UserIdsList.add(2);
+        UserIdsList.add(3);
+        UserIdsList.add(4);
+        RemoveGroupMembersResponse expected = RemoveGroupMembersResponse.newBuilder().setIsSuccess(true).setMessage("Users: " + UserIdsList + " removed.").build();
+        RemoveGroupMembersRequest.Builder request = RemoveGroupMembersRequest.newBuilder()
+                .setGroupId(1);
+
+        for (Integer userId : UserIdsList) {
+            request.addUserIds(userId);
+        }
+
+        RemoveGroupMembersRequest expectedInput = request.build();
+        RemoveGroupMembersRequest fakeRequest = RemoveGroupMembersRequest.newBuilder().build();
+
+
+
+        when(groupServiceStub.removeGroupMembers(expectedInput)).thenReturn(expected);
+        RemoveGroupMembersResponse actual = groupServiceStub.removeGroupMembers(fakeRequest);
+        assertNotEquals(expected, actual);
     }
 
 }
