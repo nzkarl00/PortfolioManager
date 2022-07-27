1cb67268d653d0c7cce3e7444af9c1c8280844ba
Lane Edwards-Brown <led43@uclive.ac.nz>
2022-07-27 16:31:50 +1200

    Added the basic template into the application (with temporary routing), and added the basics of the collapsible structure to get populated (template and data population not added).

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EvidenceListController.java
@@ -0,0 +1,80 @@
+package nz.ac.canterbury.seng302.portfolio.controller;
+
+import nz.ac.canterbury.seng302.portfolio.model.Project;
+import nz.ac.canterbury.seng302.portfolio.model.ProjectRepository;
+import nz.ac.canterbury.seng302.portfolio.service.AccountClientService;
+import nz.ac.canterbury.seng302.portfolio.service.AuthStateInformer;
+import nz.ac.canterbury.seng302.portfolio.service.ProjectService;
+import nz.ac.canterbury.seng302.portfolio.service.SprintService;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * responsible for the main/landing page of the project(s)
+ */
+@Controller
+public class EvidenceListController {
+
+  @Autowired
+  private ProjectRepository repository;
+  @Autowired
+  private ProjectService projectService;
+  @Autowired
+  private SprintService sprintService;
+  @Autowired
+  private AccountClientService accountClientService;
+  @Autowired
+  private NavController navController;
+
+  /**
+   * Directs the user to the landing project page
+   * @param principal
+   * @param model The model to be used by the application for web integration
+   * @return redirects to the landing page
+   * @throws Exception
+   */
+  @GetMapping("/evidence")
+  public String evidenceListController( @AuthenticationPrincipal AuthState principal, Model model) throws Exception {
+
+    List<Project> projectList = projectService.getAllProjects();
+    if (projectList.isEmpty()) {
+      String thisYear = new SimpleDateFormat("yyyy").format(new Date());
+      Project project = new Project("Project "+thisYear, "", LocalDate.now(),
+              LocalDate.now().plusMonths(8));
+      repository.save(project);
+    }
+    projectList = projectService.getAllProjects();
+    model.addAttribute("projects", projectList);
+
+    Integer id = AuthStateInformer.getId(principal);
+
+    // Attributes For header
+    UserResponse userReply;
+    userReply = accountClientService.getUserById(id);
+    navController.updateModelForNav(principal, model, userReply, id);
+
+    // End of Attributes for header
+
+    String role = AuthStateInformer.getRole(principal);
+
+    if (role.equals("teacher") || role.equals("admin")) {
+      model.addAttribute("display", "");
+    } else {
+      model.addAttribute("display", "display:none;");
+    }
+
+    return "evidenceList";
+  }
+
+}
