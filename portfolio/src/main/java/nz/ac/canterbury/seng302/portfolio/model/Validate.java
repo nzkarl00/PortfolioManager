4e425f390c333461c0b8581b0b5f033cd7ecc38d
Toby Morgan <tmo89@uclive.ac.nz>
2022-07-29 15:41:59 +1200

    Created backend/model for validation

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/Validate.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/Validate.java
@@ -1,19 +1,30 @@
 package nz.ac.canterbury.seng302.portfolio.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validate {
 
+    /**
+     * Basic text checker, checks there are at least 3 non-special characters from any language
+     * @param text The text to check
+     */
     public static void validateText(String text) {
-        //Traverse the string
-        int character_count = 0;
-        for(int i = 0, i < text.length(); i++) {
-            //Check
-            if()
-                character_count++;
+        int charCount = 0;
+        //This pattern will match characters from any language
+        //https://stackoverflow.com/a/64293069
+        Pattern charPattern = Pattern.compile("[\\p{L}]");
+        //Traverse the string and check each character individually
+        for(int i = 0; i < text.length(); i++) {
+            String charString = String.valueOf(text.charAt(i));
+            Matcher charMatcher = charPattern.matcher(charString);
+            if(charMatcher.matches()) {
+                charCount++;
+            }
         }
-        //Check there are at least 3 non-special characters
-        if(character_count < 3) {
-            throw new IllegalArgumentException("Text does not contain enough characters");
+        //If there are not enough non-special characters, throw an error
+        if(charCount < 3) {
+          throw new IllegalArgumentException("Text does not contain enough non-special characters");
         }
-
     }
 }
