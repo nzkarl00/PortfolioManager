a069b479c3672e25895fd9949297ea903c54ae51
Hugo Reeves <hre56@uclive.ac.nz>
2022-08-05 16:22:53 +1200

    Working weblink checker, time to integrate into controllers

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/WebLinkClient.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/WebLinkClient.java
@@ -4,15 +4,22 @@ package nz.ac.canterbury.seng302.portfolio.service;
 import nz.ac.canterbury.seng302.portfolio.model.evidence.WebLink;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 import reactor.core.publisher.Mono;
 
+import java.net.UnknownHostException;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
 import java.time.Duration;
 
 /**
@@ -36,24 +43,32 @@ public class WebLinkClient {
      * @param link
      */
     public void tryLink(final WebLink link) {
-        final int notFoundCode = 404;
-        Mono<String> uriSpec = client.method(HttpMethod.GET)
+        Mono<ResponseEntity<String>> uriSpec = client.method(HttpMethod.GET)
             .uri(link.url)
             .retrieve()
             .onStatus(
-                    status -> status.value() == notFoundCode,
+                    status -> status == HttpStatus.NOT_FOUND,
                     clientResponse -> Mono.error(new Error("Status is 404"))
             )
-            .bodyToMono(String.class);
+            .toEntity(String.class);
+
         try {
-            String res = uriSpec.block();
-            logger.info(res);
-            link.setFetchResult(false);
-            return
+            ResponseEntity res = uriSpec.block();
+            logger.info(res.getStatusCode().toString());
+            logger.info(res.getHeaders().toString());
+            logger.info(res.getHeaders().getLocation().toString());
         } catch (Error e) {
             logger.warn("Requesting link resulted in a 404");
-        } catch (WebClientException e) {
-            logger.warn("Requesting link resulted in exception", e);
+        } catch (WebClientRequestException e) {
+            if (e.getCause().getClass() == UnknownHostException.class) {
+                logger.warn(String.format(
+                    "Requesting link resulted in unknown host error, link: %s",
+                    link.url
+                ));
+            } else {
+                logger.warn("Requesting link resulted in exception", e);
+            }
+            link.setFetchResult(false, true);
         }
     }
 
