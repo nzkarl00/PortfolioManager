d59e97bed77ab37106af262609cecaf268a98671
Zoher Hussein <zhu45@uclive.ac.nz>
2022-08-06 23:36:41 +0000

    Save And Cancel

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EvidenceListController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EvidenceListController.java
@@ -5,10 +5,7 @@ import nz.ac.canterbury.seng302.portfolio.model.evidence.EvidenceRepository;
 import nz.ac.canterbury.seng302.portfolio.model.Project;
 import nz.ac.canterbury.seng302.portfolio.model.ProjectRepository;
 import nz.ac.canterbury.seng302.portfolio.model.evidence.*;
-import nz.ac.canterbury.seng302.portfolio.service.AccountClientService;
-import nz.ac.canterbury.seng302.portfolio.service.AuthStateInformer;
-import nz.ac.canterbury.seng302.portfolio.service.ProjectService;
-import nz.ac.canterbury.seng302.portfolio.service.SprintService;
+import nz.ac.canterbury.seng302.portfolio.service.*;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
 import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,13 +15,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
+
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * responsible for the main/landing page of the project(s)
@@ -33,21 +35,21 @@ import java.util.List;
 public class EvidenceListController {
 
   @Autowired
-  private ProjectRepository repository;
-  @Autowired
   private EvidenceRepository evidencerepository;
   @Autowired
-  private EvidenceTagRepository evidencetagrepository;
+  private EvidenceTagRepository evidenceTagRepository;
   @Autowired
-  private SkillTagRepository skillrepository;
+  private SkillTagRepository skillRepository;
   @Autowired
   private ProjectService projectService;
-  @Autowired
-  private SprintService sprintService;
   @Autowired
   private AccountClientService accountClientService;
   @Autowired
   private NavController navController;
+
+  private String errorMessage = "";
+
+  Logger logger = LoggerFactory.getLogger(EvidenceListController.class);
 
   /**
    * Directs the user to the landing project page
@@ -58,16 +60,18 @@ public class EvidenceListController {
    */
   @GetMapping("/evidence")
   public String evidenceListController( @AuthenticationPrincipal AuthState principal,
-                                        @RequestParam(required = false , value="ui") Integer user_id,
-                                        @RequestParam(required = false , value="pi") Integer project_id,
-                                        @RequestParam(required = false , value="si") Integer skill_id,
-                                        @RequestParam(required = false , value="ci") Integer category_id,
+                                        @RequestParam(required = false , value="ui") Integer userId,
+                                        @RequestParam(required = false , value="pi") Integer projectId,
+                                        @RequestParam(required = false , value="si") Integer skillId,
+                                        @RequestParam(required = false , value="ci") Integer categoryId,
                                         Model model) throws Exception {
+    logger.info(String.format("Fetching evidence details"));
+
     List<Evidence> evidenceList;
 
-    evidenceList = getEvidenceFunction(user_id, project_id, category_id, skill_id);
+    evidenceList = getEvidenceFunction(userId, projectId, categoryId, skillId);
 
-    List<SkillTag> skillList = skillrepository.findAll();
+    List<SkillTag> skillList = skillRepository.findAll();
 
     model.addAttribute("evidenceList", evidenceList);
     model.addAttribute("skillList", skillList);
@@ -78,40 +82,103 @@ public class EvidenceListController {
     UserResponse userReply;
     userReply = accountClientService.getUserById(id);
     navController.updateModelForNav(principal, model, userReply, id);
-
     // End of Attributes for header
-
-    String role = AuthStateInformer.getRole(principal);
-
-    if (role.equals("teacher") || role.equals("admin")) {
-      model.addAttribute("display", "");
-    } else {
-      model.addAttribute("display", "display:none;");
+    //Attributes for form
+    boolean showForm = false;
+    if (projectId != null) {
+      showForm = true;
+      model.addAttribute("date", DateParser.dateToStringHtml(new Date()));
+      Project project = projectService.getProjectById(projectId);
+      model.addAttribute("project", project);
     }
+    model.addAttribute("showForm", showForm);
+    model.addAttribute("errorMessage", errorMessage);
+    this.errorMessage = "";
 
     return "evidenceList";
   }
 
+
+  /**
+   * Saves a new evidence if the user has permissions and the correct input is given
+   * @param principal
+   * @param title evidence title
+   * @param date evidence date
+   * @param projectId the id of the project that the evidence is linked too
+   * @param evidenceCategory the category the evidence is associated with
+   * @param skills the skills the evidence is associated with
+   * @param description evidence description
+   * @param model The model to be used by the application for web integration
+   * @return redirect to the evidence page
+   * @throws Exception
+   */
+  @PostMapping("/add-evidence")
+  public String newEvidence(
+          @AuthenticationPrincipal AuthState principal,
+          @RequestParam(value = "titleInput") String title,
+          @RequestParam(value = "dateInput") String date,
+          @RequestParam(value = "projectId") Integer projectId,
+          @RequestParam(value = "evidenceCategory") Optional <String> evidenceCategory,
+          @RequestParam(value = "skillsInput") Optional <String> skills,
+          @RequestParam(value = "linksInput") Optional <String> links,
+          @RequestParam(value = "descriptionInput") String description,
+          Model model
+  ) throws Exception {
+    logger.info(String.format("Attempting to add new evidence"));
+    this.errorMessage = "";
+
+    // https://stackoverflow.com/questions/14278170/how-to-check-whether-a-string-contains-at-least-one-alphabet-in-java
+    // Checks if there is at least one character in title
+    if(!(title.matches(".*[a-zA-Z]+.*")) || title.length() <= 1) {
+      errorMessage = "Title must more than one character and should not be only made from numbers and symbols";
+    }
+
+    // Checks if there is at least one character in description
+    if(!(description.matches(".*[a-zA-Z]+.*")) || description.length() <= 1) {
+      errorMessage = "Description must more than one character and should not be only made from numbers and symbols";
+    }
+
+    Integer accountID = AuthStateInformer.getId(principal);
+    Project parentProject = projectService.getProjectById(projectId);
+    LocalDate evidenceDate = LocalDate.parse(date);
+    LocalDate projectStartDate = parentProject.getLocalStartDate();
+    LocalDate projectEndDate = parentProject.getLocalEndDate();
+
+    // Check if the given evidence date is within the project date
+    if (!(evidenceDate.isAfter(projectStartDate) && evidenceDate.isBefore(projectEndDate))
+            && !(evidenceDate.isEqual(projectEndDate) || evidenceDate.isEqual(projectStartDate))) {
+      errorMessage = "Dates must fall within project dates";
+    }
+    // If no error occurs then save the evidence to the repo
+    if(errorMessage.equals("")) {
+      Evidence evidence = new Evidence(accountID, parentProject, title, description, evidenceDate);
+      evidencerepository.save(evidence);
+      logger.info(String.format("Evidence has been created and saved to the repo evidenceId=<%s>", evidence.getId()));
+      errorMessage = "Evidence has been added";
+    }
+    model.addAttribute("errorMessage", errorMessage);
+    return "redirect:evidence?pi=" + projectId;
+  }
   /**
    * Takes the parameters and returns the appropriate evidence list based on search priority
-   * @param user_id Id of user to get evidence from
-   * @param project_id Id of project to get evidence from
-   * @param category_id Id of category to get evidence from
-   * @param skill_id Id of skill to get evidence from
+   * @param userId Id of user to get evidence from
+   * @param projectId Id of project to get evidence from
+   * @param categoryId Id of category to get evidence from
+   * @param skillId Id of skill to get evidence from
    * @return A properly sorted and filtered list of evidence
    * @throws Exception
    */
-  private List<Evidence> getEvidenceFunction(Integer user_id, Integer project_id, Integer category_id, Integer skill_id) throws Exception {
+  private List<Evidence> getEvidenceFunction(Integer userId, Integer projectId, Integer categoryId, Integer skillId) throws Exception {
 
-    if (project_id != null){
-      Project project = projectService.getProjectById(Integer.valueOf(project_id));
+    if (projectId != null){
+      Project project = projectService.getProjectById(Integer.valueOf(projectId));
       return evidencerepository.findAllByAssociatedProjectOrderByDateDesc(project);
-    } else if (user_id != null){
-      return evidencerepository.findAllByParentUserIdOrderByDateDesc(Integer.valueOf(user_id));
-    }else if (category_id != null){
+    } else if (userId != null){
+      return evidencerepository.findAllByParentUserIdOrderByDateDesc(Integer.valueOf(userId));
+    }else if (categoryId != null){
       return evidencerepository.findAllByOrderByDateDesc();
-    }else if (skill_id != null){
-      List<EvidenceTag> evidenceTags = evidencetagrepository.findAllByParentSkillTagId(Integer.valueOf(skill_id));
+    }else if (skillId != null){
+      List<EvidenceTag> evidenceTags = evidenceTagRepository.findAllByParentSkillTagId(Integer.valueOf(skillId));
       List<Evidence> evidenceSkillList = new ArrayList<>();
       for (EvidenceTag tag: evidenceTags){
         evidenceSkillList.add(tag.getParentEvidence());
@@ -131,23 +198,23 @@ public class EvidenceListController {
    */
   @GetMapping("/search-evidence")
   public String searchEvidenceParam( @AuthenticationPrincipal AuthState principal,
-                                        @RequestParam(required = false , value="ui") String user_id,
-                                        @RequestParam(required = false , value="pi") String project_id,
-                                        @RequestParam(required = false , value="si") String skill_id,
-                                        @RequestParam(required = false , value="ci") String category_id,
+                                        @RequestParam(required = false , value="ui") String userId,
+                                        @RequestParam(required = false , value="pi") String projectId,
+                                        @RequestParam(required = false , value="si") String skillId,
+                                        @RequestParam(required = false , value="ci") String categoryId,
                                         Model model) throws Exception {
     String returnString = "redirect:evidence?";
-    if (user_id != null) {
-      returnString += "ui=" + (user_id);
+    if (userId != null) {
+      returnString += "ui=" + (userId);
     }
-    if (project_id != null) {
-      returnString += "pi=" + (project_id);
+    if (projectId != null) {
+      returnString += "pi=" + (projectId);
     }
-    if (skill_id != null) {
-      returnString += "si=" + (skill_id);
+    if (skillId != null) {
+      returnString += "si=" + (skillId);
     }
-    if (category_id != null) {
-      returnString += "ci=" + (category_id);
+    if (categoryId != null) {
+      returnString += "ci=" + (categoryId);
     }
 
     return returnString;
--- portfolio/src/test/resources/gradle/cucumber/evidencePage.feature
+++ portfolio/src/test/resources/gradle/cucumber/evidencePage.feature
@@ -5,3 +5,22 @@ Feature: U25: Piece of evidence creation
     And I am authenticated as a admin
     When I go to the evidence page
     Then There will be the data for the evidence I created
+
+   Scenario: AC4: Saving and cancelling give expected results. The save button is only enabled once the mandatory fields are filled in.
+     Given I am authenticated as a admin
+     And I go to the evidence page with a project id
+     And I click the Add Evidence button
+     When The all mandatory fields to an evidence are empty, I cannot click the save button
+
+     Given I am authenticated as a admin
+     And I go to the evidence page with a project id
+     And I click the Add Evidence button
+     When I click the cancel button
+     Then I can see the evidence creation page extract and replace by a plus button
+
+     Given I am authenticated as a admin
+     And I go to the evidence page with a project id
+     And I click the Add Evidence button
+     When I have filled out all mandatory title, description, and date fields to an evidence
+     And I click the save button
+     Then I will see a message that this evidence has saved successfully
