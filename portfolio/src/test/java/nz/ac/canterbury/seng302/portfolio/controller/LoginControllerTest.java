6c454d4bd9f68f6a8fe1bf62b4f7f6d5076ec392
Yiyang (Jessie) Yu <yyu69@uclive.ac.nz>
2022-09-28 10:39:53 +1300

    skeleton of testing logining in cookies #feature #test

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/LoginControllerTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/LoginControllerTest.java
@@ -1,20 +1,33 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
+import nz.ac.canterbury.seng302.portfolio.authentication.CookieUtil;
 import nz.ac.canterbury.seng302.portfolio.service.AuthenticateClientService;
 import org.junit.Before;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static nz.ac.canterbury.seng302.portfolio.common.CommonControllerUsage.testUserStudent;
+import static nz.ac.canterbury.seng302.portfolio.common.CommonControllerUsage.validAuthStateStudent;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static sun.nio.cs.Surrogate.is;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = LoginController.class)
@@ -35,6 +48,11 @@ public class LoginControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(LoginController.class).build();
     }
 
+//    @AfterAll
+//    public static void close() {
+//        utilities.close();
+//    }
+
     @Test
     public void loginMessageIsEmptyOnNavigation() throws Exception {
         mockMvc.perform(get("/login"))
@@ -43,5 +61,47 @@ public class LoginControllerTest {
                 .andExpect(MockMvcResultMatchers.model().attributeExists("loginMessage"))
                 .andExpect(MockMvcResultMatchers.model().attribute("loginMessage", ""));
     }
+
+//    @Test
+//    public void loginHasSuccessMessageAfterValidLogin() throws Exception {
+//        mockMvc.perform(post("/login"))
+//            .andExpect(status().isOk())
+//            .andExpect(MockMvcResultMatchers.model().attributeExists("loginMessage"));
+//    }
+//
+//    @Test
+//    public void setCookieValid() throws Exception {
+//
+//        // Put the cookie in the request
+//
+//        final String cookieValue = “some value”;
+//
+//        // authenticate login meessage,
+//        // response from above^^
+//
+//        MvcResult result = mockMvc
+//            .perform(post("/login")
+//                .param("username", username)
+//                .param("password", password)
+//            )
+//            .andExpect(status().isOk())
+//            .andExpect(MockMvcResultMatchers.model().attributeExists("loginMessage"))
+//            .cookie(invalidSessionCookie)
+//            .param(“configEntity”, “configEntity”));
+//
+//        assertThat(<getCookieFromRequest>,
+//            is(
+//                CookieUtil.create(
+//                response,
+//                "lens-session-token",
+//                authenticateResponse.getToken(),
+//                true,
+//                5*60*60,
+//                domain.startsWith("localhost") ? null : domain
+//                )
+//            )
+//        );
+//
+//    }
 
 }
